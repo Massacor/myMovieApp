eb78269ea8765ece61c8f4f4e4b25c84e7689ffb
MENAGER Ange <ange.menager@soprasteria.com>
2019-04-10 16:40:10 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var myMovieApp = angular.module('myMovieApp', []);
+
+myMovieApp.controller('movieCtrl', function movieCtrl($scope, $rootScope){
+
+});
+
+myMovieApp.service('callApi', ['$http', function($scope, $rootScope, $timeout){
+	
+}]);
