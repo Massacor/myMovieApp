6a0c4fc1ebf460494218668d87ef5c93919c3ed8
Massacor <apowarp@gmail.com>
2019-04-10 17:52:18 +0200

    API connexion is ok

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 var myMovieApp = angular.module('myMovieApp', []);
 
-myMovieApp.controller('movieCtrl', function movieCtrl($scope, $rootScope){
+myMovieApp.controller('moviesCtrl', function($scope, $rootScope, callApi){
+	var vm = this;
 
+	vm.search = function(){
+		$scope.promise = callApi.getMovie();
+
+		$scope.promise.then(function(result){
+			vm.infos=result;
+		});
+	};
 });
 
-myMovieApp.service('callApi', ['$http', function($scope, $rootScope, $timeout){
-	
+myMovieApp.service('callApi', ['$http', function($http, $scope){
+	var vm = this;
+
+	vm.getMovie = function() {
+		var req = {
+			method: 'GET',
+			url: 'https://api.themoviedb.org/3/movie/550?api_key=cdae4e177549fd7fa6ea0fe4d25f8ef0'
+		};
+		return $http(req).then(function(result){
+			return result.data;
+		}).catch(function(){
+			throw 'API KO';
+		});
+	}
 }]);
